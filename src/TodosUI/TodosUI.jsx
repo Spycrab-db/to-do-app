73d440c6b5efe55cc69e3076c9736c14324fcdf8
Spycrab-db <gavinyan2004@gmail.com>
2023-07-23 18:34:18 -0400

    Added fade in transition after changing currList

--- src/TodosUI/TodosUI.jsx
+++ src/TodosUI/TodosUI.jsx
@@ -1,13 +1,15 @@
 import Task from './Task';
 import NewTask from './NewTask';
 import CompletedDropdown from './CompletedDropdown';
+import './css/TodosUI.css'
+import { useRef, useEffect } from 'react';
 export default function TodosUI({ currList, allTasks, addTask, toggleComplete, deleteTask }) {
     const tasks = allTasks.filter((task) => task.parentList === currList);
     const incomplete = tasks.filter((task) => !task.completed);
     const completed = tasks.filter((task) => task.completed);
     return (
-        <div className="todo-ui">
-            <h1>{currList.title}</h1>
+        <div className="todo-ui" key={currList.title}>
+            <h1 className="list-title">{currList.title}</h1>
             <ul>
                 {incomplete.map((task) => {
                     return <Task
